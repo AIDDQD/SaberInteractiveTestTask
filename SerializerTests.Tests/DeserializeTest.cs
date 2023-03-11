35089c3c90341b54e161af1761bea12fedaeecc8
AIDDQD <artemiy.bezgin@gmail.com>
2023-03-11 16:37:39 +0300

    Десериализатор

--- SerializerTests.Tests/DeserializeTest.cs
+++ SerializerTests.Tests/DeserializeTest.cs
@@ -2,31 +2,35 @@ using System.Text;
 using SerializerTests.Implementations;
 using SerializerTests.Interfaces;
 
-namespace SerializerTests.Tests; 
+namespace SerializerTests.Tests;
 
-public class DeserializeTest {
-    private IListSerializer _serializer;
+public class DeserializeTest
+{
+    private readonly IListSerializer _serializer;
 
-    public DeserializeTest() {
+    public DeserializeTest()
+    {
         _serializer = new PseudoJsonListSerializer();
     }
-    
+
     [Fact]
-    public void Deserialize_InvalidList() {
+    public void Deserialize_InvalidList()
+    {
         var raw = @"[]";
-        
+
         using var ms = new MemoryStream();
         ms.Write(Encoding.UTF8.GetBytes(raw));
         ms.Seek(0, SeekOrigin.Begin);
-        
+
         var result = _serializer.Deserialize(ms).Result;
         Assert.Null(result);
     }
-    
+
     [Fact]
-    public void Deserialize_SingleItem() {
-        var raw = @"[{Data=""test-test"",Previous=null,Next=null,Random=null},]";
-        
+    public void Deserialize_SingleItem()
+    {
+        var raw = @"[{""Data"":""test-test"",""Previous"":null,""Next"":null,""Random"":null}]";
+
         using var ms = new MemoryStream();
         ms.Write(Encoding.UTF8.GetBytes(raw));
         ms.Seek(0, SeekOrigin.Begin);
@@ -39,9 +43,11 @@ public class DeserializeTest {
     }
 
     [Fact]
-    public void Deserialize_TwoItems() {
-        var raw = @"[{Data=""test-test"",Previous=null,Next=1,Random=null},{Data=""super-test"",Previous=0,Next=null,Random=null},]";
-        
+    public void Deserialize_TwoItems()
+    {
+        var raw =
+            @"[{""Data"":""test-test"",""Previous"":null,""Next"":1,""Random"":null},{""Data"":""super-test"",""Previous"":0,""Next"":null,""Random"":null},]";
+
         using var ms = new MemoryStream();
         ms.Write(Encoding.UTF8.GetBytes(raw));
         ms.Seek(0, SeekOrigin.Begin);
@@ -51,17 +57,19 @@ public class DeserializeTest {
         Assert.NotNull(result.Next);
         Assert.Null(result.Previous);
         Assert.Null(result.Random);
-        
+
         Assert.Equal("super-test", result.Next.Data);
         Assert.Equal(result.Data, result.Next.Previous.Data);
         Assert.Null(result.Next.Next);
         Assert.Null(result.Next.Random);
     }
-    
+
     [Fact]
-    public void Deserialize_Random() {
-        var raw = @"[{Data=""test-test"",Previous=null,Next=1,Random=1},{Data=""super-test"",Previous=0,Next=null,Random=null},]";
-        
+    public void Deserialize_Random1()
+    {
+        var raw =
+            @"[{""Data"":""test-test"",""Previous"":null,""Next"":1,""Random"":1},{""Data"":""super-test"",""Previous"":0,""Next"":null,""Random"":null},]";
+
         using var ms = new MemoryStream();
         ms.Write(Encoding.UTF8.GetBytes(raw));
         ms.Seek(0, SeekOrigin.Begin);
@@ -71,4 +79,23 @@ public class DeserializeTest {
         Assert.Null(result.Random.Next);
         Assert.Null(result.Random.Random);
     }
+
+    /*
+    // Этот тест не будет работать, поскольку в Data находится неэкранированная кавычка и ошибка здесь ожидаема.
+    // Но я его добавил и адаптировал, хотя сейчас он явно неактуален.
+    [Fact]
+    public void Deserialize_Random2()
+    {
+        var raw =
+            @"[{""Data"":"""""",""Previous"":null,""Next"":1,""Random"":1},{""Data"":""super-test"",""Previous"":0,""Next"":null,""Random"":null},]";
+
+        using var ms = new MemoryStream();
+        ms.Write(Encoding.UTF8.GetBytes(raw));
+        ms.Seek(0, SeekOrigin.Begin);
+
+        var result = _serializer.Deserialize(ms).Result;
+        Assert.Equal(result.Next.Data, result.Random.Data);
+        Assert.Null(result.Random.Next);
+        Assert.Null(result.Random.Random);
+    }*/
 }
--- SerializerTests/Implementations/ListDeserializer.cs
+++ SerializerTests/Implementations/ListDeserializer.cs
@@ -1,67 +1,62 @@
-using System.Text;
+using Newtonsoft.Json;
 using SerializerTests.Nodes;
 
 namespace SerializerTests.Implementations;
 
-internal readonly struct NodeInfo {
+internal readonly struct NodeInfo
+{
     public readonly ListNode Node;
     public readonly int? Previous;
     public readonly int? Next;
     public readonly int? Random;
 
-    public NodeInfo(ListNode node, string data, int? previous, int? next, int? random) {
+    public NodeInfo(ListNode node, string data, int? previous, int? next, int? random)
+    {
         Node = node;
         Node.Data = data;
-        
+
         Previous = previous;
         Next = next;
         Random = random;
     }
 }
 
-internal static class ListDeserializer {
-    public static ListNode Deserialize(Stream s) {
-        int firstByte = s.ReadByte();
-        if (firstByte is -1 or not '[')
-        {
-            throw new ArgumentException("Поток путой или начинается не с '['");
-        }
-        
+internal static class ListDeserializer
+{
+    public static ListNode Deserialize(Stream s)
+    {
+        using var streamReader = new StreamReader(s);
+        using var jsonReader = new JsonTextReader(streamReader);
+
+        var canRead = jsonReader.Read();
+        if (!canRead || jsonReader.TokenType != JsonToken.StartArray)
+            throw new ArgumentException("Конец потока или получен неожиданный токен", nameof(s));
+
         var nodeInfos = new List<NodeInfo>();
-        var nodeRead = false;
-        while (true)
+        try
         {
-            var b = s.ReadByte();
+            while (jsonReader.Read())
+            {
+                if (jsonReader.TokenType is not JsonToken.StartObject)
+                    continue;
 
-            if (b == '{' && !nodeRead)
-            {
-                nodeRead = true;
-                nodeInfos.Add(ParseNodeInfo(s));
-            }
-            else if (b == ',' && nodeRead)
-            {
-                nodeRead = false;
-            }
-            else if (b == -1)
-            {
-                throw new ArgumentException($"Неожиданный конец потока", nameof(s));
-            }
-            else if (b == ']')
-            {
-                break;
-            }
-            else {
-                throw new ArgumentException($"Неожиданный символ '{(char) b}'", nameof(s));
+                var nodeInfo = ParseNodeInfo(jsonReader);
+                nodeInfos.Add(nodeInfo);
             }
         }
+        catch (JsonReaderException ex)
+        {
+            throw new ArgumentException("Ошибка чтения данных из потока", ex);
+        }
 
-        return FillNodes(nodeInfos);
+        return ConnectNodes(nodeInfos);
     }
 
-    private static ListNode FillNodes(IList<NodeInfo> nodeInfos) {
+    private static ListNode ConnectNodes(IList<NodeInfo> nodeInfos)
+    {
         if (!nodeInfos.Any())
             return null;
-        
+
         foreach (var nodeInfo in nodeInfos)
         {
             if (nodeInfo.Next != null)
@@ -71,61 +66,29 @@ internal static class ListDeserializer {
             if (nodeInfo.Random != null)
                 nodeInfo.Node.Random = nodeInfos[nodeInfo.Random.Value].Node;
         }
-        
+
         return nodeInfos[0].Node;
     }
 
-    private static NodeInfo ParseNodeInfo(Stream s) {
+    private static NodeInfo ParseNodeInfo(JsonReader reader)
+    {
         var fields = new Dictionary<string, string>(4);
-        string fieldName, value;
-        for (int i = 0; i < 3; i++) {
-            (fieldName, value) = ReadField(',', s);
+        for (var i = 0; i < 4; i++)
+        {
+            if (reader.TokenType is not JsonToken.PropertyName)
+                reader.Read();
+
+            var fieldName = reader.Value?.ToString();
+            var value = reader.ReadAsString();
             fields[fieldName] = value;
         }
-        
-        (fieldName, value) = ReadField('}', s);
-        fields[fieldName] = value;
 
         return new NodeInfo(
             new ListNode(),
-            fields["Data"], 
-            fields["Previous"] == "null" ? null : int.Parse(fields["Previous"]), 
-            fields["Next"] == "null" ? null : int.Parse(fields["Next"]),
-            fields["Random"] == "null" ? null : int.Parse(fields["Random"])
-            );
+            fields["Data"],
+            fields["Previous"] == null ? null : int.Parse(fields["Previous"]),
+            fields["Next"] == null ? null : int.Parse(fields["Next"]),
+            fields["Random"] == null ? null : int.Parse(fields["Random"])
+        );
     }
-    
-    private static (string FieldName, string Value) ReadField(char stopChar, Stream s)
-    {
-        var line = ReadUntil(s, stopChar);
-        var parts = line.Split('=', 2);
-        return (parts[0], parts[1]);
-    }
-    
-    private static string ReadUntil(Stream s, char stopChar)
-    {
-        var sb = new StringBuilder();
-        while (true)
-        {
-            var b = s.ReadByte();
-            
-            if ((char) b == stopChar)
-                break;
-
-            switch (b) {
-                case '\'':
-                    sb.Append(ReadUntil(s, '\''));
-                    break;
-                case '"':
-                    sb.Append(ReadUntil(s, '"'));
-                    break;
-                default:
-                    sb.Append((char) b);
-                    break;
-            }
-        }
-
-        return sb.ToString();
-    }
-    
 }
